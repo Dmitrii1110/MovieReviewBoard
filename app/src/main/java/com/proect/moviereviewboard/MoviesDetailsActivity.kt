666ea8056eb5dd349c24f046c1483fc464fddbf0
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-01-27 16:23:09 +0300

    Подготовил xml экран информации о фильме

---
+++ app/src/main/java/com/proect/moviereviewboard/MoviesDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.proect.moviereviewboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoviesDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_details)
+    }
+}
