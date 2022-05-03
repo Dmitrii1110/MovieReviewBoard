7fb2d31ed2083436d792c90784c0133a6d99796e
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-05-03 16:33:27 +0300

    Внёс правки в структуре файлов

--- app/src/main/java/com/proect/moviereviewboard/Constants.kt
+++ app/src/main/java/com/proect/moviereviewboard/Constants.kt
@@ -3,6 +3,6 @@ package com.proect.moviereviewboard
 class Constants {
 
     companion object {
-        const val API_KEY = "2439cc49a36685a90f1febdc8680c95a"
+        const val API_KEY = ""
     }
 }
